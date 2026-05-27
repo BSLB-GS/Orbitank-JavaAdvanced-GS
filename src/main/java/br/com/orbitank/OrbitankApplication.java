72827c4ed4b1e403dacbd5d5c336f445091586b7
manuelalacerda <manuelalacerda88@gmail.com>
2026-05-27 18:17:23 -0300

    chore: initialize spring boot project

---
+++ src/main/java/br/com/orbitank/OrbitankApplication.java
@@ -0,0 +1,13 @@
+package br.com.orbitank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/orbitank/OrbitankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orbitank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
