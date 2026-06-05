193f4914fa2d02f0d103dfb2224b2c4df6b99986
manuelalacerda <manuelalacerda88@gmail.com>
2026-06-05 17:34:40 -0300

    feat: criando a entidade TankOperationalAlert

--- src/main/java/br/com/orbitank/entity/TankOperationalAlert.java
+++ src/main/java/br/com/orbitank/entity/TankOperationalAlert.java
@@ -1,4 +1,22 @@
 package br.com.orbitank.entity;
 
-public class TankOperationalAlert {
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@DiscriminatorValue("TANK")
+public class TankOperationalAlert extends OperationalAlert {
+
+    @Column(name="tank_identifier",length=50)
+    private String tankIdentifier;
+
+    @Column(name="resource_type",length=50)
+    private String resourceType;
 }
